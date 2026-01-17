cddb8f398c97ad18f5418a462172ef430dc61688
Peehu Mishra <peehumishra1308@gmail.com>
2026-01-17 18:25:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Homescreen from './screen/Homescreen'
+import Blogs from './components/Blog'
 
 function App() {
   
 
   return (
     <div className='bg-[#F4F2F2] min-h-screen w-full top-0 '>
-      <Navbar/>
+      
       <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Homescreen/>}/>
+        <Route path="/blog" element={<Blogs/>}/>
       </Routes>
       </BrowserRouter>
       
